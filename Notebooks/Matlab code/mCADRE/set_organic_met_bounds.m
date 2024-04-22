1eaf3d090cc9b9da94e63bcf150d7bf664880817
pablodig <48334248+pablodig@users.noreply.github.com>
2024-04-22 15:15:30 -0700

    New version of mCADRE

--- Notebooks/Matlab code/mCADRE/set_organic_met_bounds.m
+++
@@ -1,8 +0,0 @@
-function model = set_organic_met_bounds(model, exRxns)
-
-% Identify all exchange reactions that include organic metabolites
-organicExRxns = find_organic_ex_rxns(model, exRxns);
-
-% Reset all lower bounds for organic reactions to 0 to turn off uptake
-model = changeRxnBounds(model, organicExRxns, 0, 'l');
-
